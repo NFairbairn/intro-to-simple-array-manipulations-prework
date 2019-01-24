325c03a9cd3249178f3d72acd8f40dd5b602e1d8
Noah Fairbairn <noahfairbairn@gmail.com>
2019-01-24 18:02:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -41,3 +41,8 @@ end
 def using_delete (instructors, no_offense_steven)
   instructors.delete(no_offense_steven)
 end
+
+def using_delete_at (famous_robots, int)
+  famous_robots.delete_at(int)
+end
+  
