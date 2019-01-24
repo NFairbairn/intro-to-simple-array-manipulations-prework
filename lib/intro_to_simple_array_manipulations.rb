95d4dca6f8b2e9668fc6283f13af89292d98f5dc
Noah Fairbairn <noahfairbairn@gmail.com>
2019-01-24 17:31:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,7 @@
 def using_push (countries_in_western_africa, next_country)
   countries_in_western_africa.push(next_country)
 end
-  
+
+def using_unshift (countries_in_western_africa, next_country)
+  countries_in_western_africa.unshift(next_country)
+end
